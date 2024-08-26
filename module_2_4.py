bed60a7927aa82bae66642b7d43f6b399bacf56a
Igrek141 <igor141@mail.ru>
2024-08-27 00:18:26 +0300

    27/08/24

---
+++ module_2_4.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in range(2, len(numbers) + 1):
+    is_prime = 0
+    for j in range(1, i + 1):
+        b = i % j
+        if b == 0:
+            is_prime += 1
+    if is_prime == 2:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print('Primes:    ', primes)
+print('Not Primes:', not_primes)
